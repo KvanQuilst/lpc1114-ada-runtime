4480f9f5b0d74c9100d787063912197489fd48f8
KvanQuilst <eskew.dylan@outlook.com>
2022-11-10 10:09:00 -0800

    forgot some files

---
+++ gnat/s-bbbopa.ads
@@ -0,0 +1,12 @@
+pragma Style_Checks (off);
+
+package System.BB.Board_Parameters is
+	pragma No_Elaboration_Code_All;
+	pragma Pure;
+
+  --------------------
+  -- Hardware Clock --
+  --------------------
+  Main_Clock_Frequency : constant := 48_000_000;
+
+end System.BB.Board_Parameters;
